99a5622f9b17bbb8bbbd2439f9dcb7ed7e1740f5
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2022-11-03 22:20:37 +0530

    verify User , Verify Admin middleware done

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,19 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/User.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verify.js";
 
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("you are loggedin");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("you are loggedin and you have now control your account");
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("Hello Admin and you have now control ALL account");
+});
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
 router.patch("/:id", updateUser);
---
+++ utils/verify.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  //   console.log(req.cookies.access_token);
+  if (!token) {
+    return next(createError("401", "you are not authenticated"));
+  }
+  jwt.verify(token, "test", (err, user) => {
+    if (err) return next(createError("401", "Token is not valid"));
+    // console.log(user);
+    req.user = user;
+    next();
+  });
+};
+
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id) {
+      //   console.log(req.user.id);
+      //   console.log(req.params.id);
+      next();
+    } else {
+      return next(createError("403", "You are not authorized"));
+    }
+  });
+};
+
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      console.log(req.user.isAdmin);
+      next();
+    } else {
+      return next(createError("403", "Sorry you are not a ADMIN"));
+    }
+  });
+};
